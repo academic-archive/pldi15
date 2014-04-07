959c2adaa81ec89890e8b177b4011e2265e2684e
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2014-04-07 12:16:17 -0400

    start generating contraints

--- src/Hood.ml
+++ src/Hood.ml
@@ -44,12 +44,14 @@ module Idx : sig
   type t
   val const : t
   val dst : var * var -> t
+  (* val extra : int -> t *)
   val compare : t -> t -> int
   val fold : ('a -> t -> 'a) -> 'a -> var list -> 'a
 end = struct
-  type t = Const | Dst of var * var
+  type t = Const | Dst of var * var | Extra of int
   let const = Const
   let dst (u, v) = if u < v then Dst (u, v) else Dst (v, u)
+  let extra i = Extra i
   let compare = compare
   let fold f a vl =
     let rec pairs a vl =
@@ -68,26 +70,91 @@ end
 
 module Q(C: CLPSTATE) : sig
   type ctx
-  type ineq = Eq | Le
   val create : var list -> ctx
+  val set : ctx -> Idx.t -> (Idx.t * int) list -> int -> ctx
+  val solve : ctx -> unit
 end = struct
+  module S = Set.Make(struct
+    type t = var
+    let compare = compare
+  end)
   module M = Map.Make(Idx)
-  type ctx = { cvars : var list; cmap : int M.t }
+  type ctx = { cvars : S.t; cmap : int M.t }
+
   let newv () =
-    let v = !C.next_var in
     let rows = Clp.number_rows C.state in
     let cols = Clp.number_columns C.state in
     Clp.resize C.state rows (cols + 1);
-    incr C.next_var;
-    v
+    let v = !C.next_var in
+    incr C.next_var; v
+
   let create vl =
-    { cvars = vl
-    ; cmap = Idx.fold
-        (fun m i -> M.add i (newv ()) m)
-        M.empty vl
-    }
-  type ineq = Eq | Le
+    let cvars = List.fold_left
+      (fun s v -> S.add v s)
+      S.empty vl in
+    let cmap = Idx.fold
+      (fun m i -> M.add i (newv ()) m)
+      M.empty (S.elements cvars) in
+    { cvars; cmap }
+
+  let set c idx l const =
+    let v' = newv () in
+    let row_elements = Array.of_list begin
+      (v', -1.) :: List.map
+        (fun (i, w) -> ((M.find i c.cmap), float_of_int w)) l
+    end in
+    let k = float_of_int (-const) in
+    Clp.add_rows C.state [|
+      { Clp.row_lower = k
+      ; Clp.row_upper = k
+      ; row_elements }
+    |];
+    { cvars = c.cvars; cmap = M.add idx v' c.cmap }
+
+  let solve c =
+    let obj = Clp.objective_coefficients C.state in
+    Idx.fold (fun () i -> obj.(M.find i c.cmap) <- 1.) ()
+      (S.elements c.cvars);
+    Clp.change_objective_coefficients C.state obj;
+    Clp.primal C.state
+
 end
+
+
+let rec pvars p =
+  (* get all variables used in a program (with duplicates) *)
+  let cvars (Cond (v1, v2, _)) = [v1; v2] in
+  match p with
+  | PSkip _ -> []
+  | PAssert (c, _) -> cvars c
+  | PSet (id, v2, _) -> [VId id; v2]
+  | PInc (id, _, v, _) -> [VId id; v]
+  | PSeq (p1, p2, _) -> pvars p1 @ pvars p2
+  | PWhile (c, p, _) -> cvars c @ pvars p
+
+let go cost p =
+  (* generate and resolve constraints *)
+  let clp_state = Clp.create () in
+  let module Q = Q(struct
+    let state = clp_state
+    let next_var = ref 0
+  end) in
+  let open Idx in
+  let open Eval in
+
+  let rec gen qpost = function
+    | PSkip _ -> Q.set qpost const [(const, 1)] (cost CSkip)
+    | PAssert _ -> Q.set qpost const [(const, 1)] (cost CAssert)
+    | PSeq (p1, p2, _) ->
+      let qpre1 = gen qpost p1 in
+      let qmid = Q.set qpre1 const [(const, 1)] (cost CSeq2) in
+      let qpre2 = gen qmid p2 in
+      let qpre = Q.set qpre2 const [(const, 1)] (cost CSeq1) in
+      qpre
+    | _ -> failwith "not implemented (gen)" in
+
+  let qpre = gen (Q.create (VNum 0 :: pvars p)) p in
+  Q.solve qpre
 
 
 let _ =
@@ -101,3 +168,9 @@ let _ =
     let { lpre; lpost } = UidMap.find id l in
     print_string "\n"; Logic.pp_aps lpost
   in Parse.pp_prog_hooks pre post p
+
+let _ =
+  if Array.length Sys.argv > 1 && Sys.argv.(1) = "-tq" then
+  let p = Parse.pa_prog stdin in
+  (* let l = create_logctx p in *)
+  go (function x -> Eval.atomic_ops x) p
