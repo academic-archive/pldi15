ba76e7e28df085dfb671f95c352f2026cffd4d0a
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2014-04-17 19:24:09 -0400

    new logic module

--- src/Logic.ml
+++ src/Logic.ml
@@ -2,112 +2,98 @@
 
 open Parse
 
-(*
-  The meaning of the assertion ALt l1 l2 k
-  is \sum l1 < \sum l2 + k.
-  An abstract program state is simply a list
-  of assertions.
-*)
-type assn = ALt of id list * id list * int
+module Id = struct type t = id  let compare = compare end
+module S = Set.Make(Id)
+module M = struct
+  include Map.Make(Id)
+  let coeff id m =
+    try find id m with Not_found -> 0
+  let set = add
+  let add id n m =
+    let c = coeff id m in
+    set id (c+n) m
+  let vars vs m =
+    fold (fun k c vs -> S.add k vs) m vs
+end
+
+type assn = A of int M.t * int
 type aps = assn list
 
-let rec drop elt = function
-  | e :: es -> if e = elt then es else e :: drop elt es
-  | [] -> raise Not_found
-
-let add_l1 v (ALt (l1, l2, k)) =
+let add c v (A (m, k) as assn) =
+  if c = 0 then assn else
   match v with
-  | VNum n -> ALt (l1, l2, -n + k)
-  | VId id ->
-    try ALt (l1, drop id l2, k)
-    with Not_found -> ALt (id :: l1, l2, k)
+  | VNum n -> A (m, k - c * n)
+  | VId x -> A (M.add x c m, k)
 
-let add_l2 v (ALt (l1, l2, k)) =
-  match v with
-  | VNum n -> ALt (l1, l2, n + k)
-  | VId id ->
-    try ALt (drop id l1, l2, k)
-    with Not_found -> ALt (l1, id :: l2, k)
+let assn_of_cond (Cond (v1, v2, k) (* v2 - v1 <= - k - 1 *)) =
+  add 1 v2 (add (-1) v1(A (M.empty, -k - 1)))
 
-let assn_of_cond (Cond (v1, v2, k) (* v2 < v1 - k *)) =
-  add_l1 v2 (add_l2 v1 (ALt ([], [], -k)))
+let assn_negate (A (m, k)) =
+  A (M.map (fun n -> -n) m, -k - 1)
 
-let assn_negate (ALt (l1, l2, k)) = ALt (l2, l1, 1 - k)
-
-let assn_incr id op delta (ALt (l1, l2, _) as asn) =
-  let add1, add2 =
-    match op with
-    | OPlus -> add_l1, add_l2
-    | OMinus -> add_l2, add_l1 in
-  let mem1 = List.mem id l1 in
-  let mem2 = List.mem id l2 in
-  if mem1 && mem2 then
-    failwith "assertion invariant broken"
-  else if mem1 then add2 delta asn
-  else if mem2 then add1 delta asn
-  else asn
+let assn_incr id op delta (A (m, _) as assn) =
+  let s = match op with OPlus -> -1 | OMinus -> +1 in
+  add (s * M.coeff id m) delta assn
 
 let aps_incr id op delta = List.map (assn_incr id op delta)
 
 let aps_set id v ps =
   (* forget everything concerning the assigned variable *)
-  let rec purge = function
-    | ALt (l1, l2, _) as assn :: assns ->
-      if List.mem id l1 || List.mem id l2
-        then purge assns
-        else assn :: purge assns
-    | [] -> [] in
-  add_l1 v (ALt ([], [id], 1)) :: add_l2 v (ALt ([id], [], 1)) ::
-    purge ps
+  add 1 v (add (-1) (VId id) (A (M.empty, 0))) ::
+  add (-1) v (add 1 (VId id) (A (M.empty, 0))) ::
+  List.filter (fun (A (m, _)) -> M.coeff id m = 0) ps
 
 
 (* poor man's decision procedure *)
 
-module IdSet = Set.Make(struct type t = id  let compare = compare end)
+let rec vars = function
+  | A (m, _) :: assns -> M.vars (vars assns) m
+  | [] -> S.empty
 
-let vars ps =
-  let f = List.fold_left (fun a b -> IdSet.add b a) in
-  let vs =
-    List.fold_left
-      (fun s (ALt (l1, l2, _)) -> (f (f s l2) l1))
-      IdSet.empty
-      ps in
-  IdSet.elements vs
+let rec gcd a b =
+  if a = 0 || b = 0 then a + b else
+  if a < b
+    then gcd a (b mod a)
+    else gcd (a mod b) b
 
-let assn_combine (ALt (l11, l21, k1)) (ALt (l12, l22, k2)) =
-  List.fold_left (fun assn id1 -> add_l1 (VId id1) assn)
-    (List.fold_left (fun assn id2 -> add_l2 (VId id2) assn)
-      (ALt (l11, l21, k1 + k2 - 1)) l22)
-    l12
+let assn_combine (c1, A (m1, k1)) (c2, A (m2, k2)) =
+  let c1, c2 = let g = gcd c1 c2 in (c1/g, c2/g) in
+  let h = function Some n -> n | None -> 0 in
+  let f _ a b =
+    let o = c2 * h a + c1 * h b in
+    if o = 0 then None else Some o in
+  A (M.merge f m1 m2, k1 + k2)
 
 let elim id ps =
-  let rec part3 lo hi rest  = function
-    | [] -> (lo, hi, rest)
-    | ALt (l1, l2, _) as assn :: ps' ->
-      if List.mem id l1 then part3 (assn :: lo) hi rest ps'
-      else if List.mem id l2 then part3 lo (assn :: hi) rest ps'
-      else part3 lo hi (assn :: rest) ps' in
-  let lo, hi, rest = part3 [] [] [] ps in
+  let rec part3 p n rest  = function
+    | [] -> (p, n, rest)
+    | A (m, _) as assn :: ps' ->
+      let c = M.coeff id m in
+      if c > 0 then part3 ((c, assn) :: p) n rest ps'
+      else if c < 0 then part3 p ((-c, assn) :: n) rest ps'
+      else part3 p n (assn :: rest) ps' in
+  let p, n, rest = part3 [] [] [] ps in
   let prod f l1 l2 =
     List.fold_left (* product function on lists *)
       (fun x a -> List.fold_left (fun y b -> f a b :: y) x l2)
       [] l1 in
-  prod assn_combine lo hi @ rest
+  let comb = prod assn_combine p n in
+  assert (List.for_all (fun (A (m, _)) -> M.coeff id m = 0) comb); (* XXX *)
+  comb @ rest
 
 let aps_is_valid ps =
-  let simpl = List.fold_left (fun a b -> elim b a) ps (vars ps) in
-  let dec_simple = function
-    | ALt ([], [], k) -> 0 < k
-    | _ -> failwith "Logic: bug in aps_is_valid" in
+  let simpl = S.fold elim (vars ps) ps in
+  let dec_simple (A (m, k)) =
+    assert (M.for_all (fun _ c -> c = 0) m); (* XXX *)
+    0 <= k in
   List.fold_left (fun b assn -> dec_simple assn && b) true simpl
 
 
 (* applications *)
 
 let aps_add a ps =
-  if not (aps_is_valid (assn_negate a :: ps))
-    then ps (* do not add consequences *)
-    else a :: ps
+  (* do not add consequences *)
+  if not (aps_is_valid (assn_negate a :: ps)) then ps else a :: ps
 
 let rec aps_fix ps f =
   let x, ps' = f ps in
@@ -122,18 +108,15 @@ let rec aps_fix ps f =
 
 let aps_entails ps x op delta u =
   (* check if ps entails x `op` delta \in [x, u] U [u, x] *)
-  let delta_l1, delta_l2 =
-    match op with
-    | OPlus -> add_l1 delta, add_l2 delta
-    | OMinus -> add_l2 delta, add_l1 delta in
-  (* x `op` delta < x  /\  x `op` delta < u *)
+  let s = match op with OPlus -> +1 | OMinus -> -1 in
+  (* x `op` delta <= x - 1  /\  x `op` delta <= u - 1 *)
   let disj1 =
-    [ delta_l1 (ALt ([], [], 0))
-    ; delta_l1 (add_l2 u (ALt ([x], [], 0))) ] in
-  (* u < x `op` delta  /\  x < x `op` delta *)
+    [ add s delta (A (M.empty, -1))
+    ; add s delta (add (-1) u (add 1 (VId x) (A (M.empty, -1)))) ] in
+  (* u <= x `op` delta - 1  /\  x <= x `op` delta - 1 *)
   let disj2 =
-    [ delta_l2 (add_l1 u (ALt ([], [x], 0)))
-    ; delta_l2 (ALt ([], [], 0)) ] in
+    [ add (-s) delta (add 1 u (add (-1) (VId x) (A (M.empty, -1))))
+    ; add (-s) delta (A (M.empty, -1)) ] in
   (* check entailment by refutation *)
   not (aps_is_valid (disj1 @ ps)) && not (aps_is_valid (disj2 @ ps))
 
@@ -143,17 +126,26 @@ let pp_aps ps =
   let rec ppl z sep pp = function
     | [] -> print_string z
     | [x] -> pp x
-    | x :: xs -> pp x; print_string sep; ppl z sep pp xs in
-  let ppa (ALt (l1, l2, k)) =
-    ppl "0" " + " print_string l1;
-    print_string " < ";
-    if l2 = [] then print_int k else begin
-      ppl "0" " + " print_string l2;
-      if k < 0 then (print_string " - "; print_int (-k))
-      else if k > 0 then (print_string " + "; print_int k)
-    end in
+    | x :: ((y :: _) as xs) ->
+      pp x; print_string (sep y);
+      ppl z sep pp xs in
+  let ppa (A (m, k)) =
+    let sep (_, c) =
+      if c < 0 then " - "
+      else if c > 0 then " + "
+      else "" in
+    let p (k, c) =
+      let c = abs c in
+      if c <> 1 then (print_int c; print_string " ");
+      print_string k in
+    let bdgs = M.bindings m in
+    (try if snd (List.hd bdgs) < 0 then print_string "-"
+    with Failure _ -> ());
+    ppl "0" sep p bdgs;
+    print_string " <= ";
+    print_int k in
   print_string "{ ";
-  ppl "True" " /\\ " ppa ps;
+  ppl "True" (fun _ -> " /\\ ") ppa ps;
   print_string " }"
 
 
@@ -164,10 +156,10 @@ let _ =
   (* semantics of assertions *)
   let get h v = List.assoc v h in
   let rec sum h = function
-    | id :: ids -> get h id + sum h ids
+    | (id, c) :: ids -> c * get h id + sum h ids
     | [] -> 0 in
-  let assn_sem h (ALt (l1, l2, k)) =
-    sum h l1 < sum h l2 + k in
+  let assn_sem h (A (m, k)) =
+    sum h (M.bindings m) <= k in
   let rec sem h = function
     | a :: al -> assn_sem h a && sem h al
     | [] -> true in
